78a3f9fe47d1ee0a28e08301ca40798fe3cf93cb
Peter Rosser <tharax@gmail.com>
2015-03-26 23:14:39 +0000

    trying to reduce code

--- binary_LED.ino
+++ binary_LED.ino
@@ -1,12 +1,8 @@
-long interval = 10;
-long pause = 1200 * interval;
-long singleTime = 0, doubleTime = 0, quadTime = 0, eightTime = 0;
-int one = 0, two = 0, three = 0, four= 0;
+long interval = 1000, singleTime = 0, doubleTime = 0, quadTime = 0, eightTime = 0;
 
 void setup()
 {
-	Serial.begin(9600);
-	Serial.println("Start");
+	//Serial.begin(9600);
 	setUpPin(13);
 	setUpPin(12);
 	setUpPin(11);
@@ -16,45 +12,20 @@ void setup()
 void loop()
 {
 	long timeNow = millis();
-	if (timeNow - singleTime >= interval)
-	{
-		singleTime += interval;
-		switchLightBulb(13);
-		one++;
-	}
-	if (timeNow - doubleTime >= 2 * interval)
-	{
-		doubleTime += (2 * interval);
-		switchLightBulb(12);
-		two++;
-	}
-	if (timeNow - quadTime >= 4 * interval)
-	{
-		quadTime += (4 * interval);
-		switchLightBulb(11);
-		three++;
-	}
-	if (timeNow - eightTime >= 8 * interval)
-	{
-		eightTime += (8 * interval);
-		switchLightBulb(9);
-		four++;
-	}
-	if (timeNow >= pause)
-	{
-		Serial.println("counters");
-		Serial.println(one);
-		Serial.println(two);
-		Serial.println(three);
-		Serial.println(four);
-		delay(600000);
-		timeNow = 0;
-	}
+	checkTimer(timeNow, singleTime, interval, 13);
+	//checkTimer(timeNow, doubleTime, 2 * interval, 12);
+	//checkTimer(timeNow, quadTime, 4 * interval, 11);
+	//checkTimer(timeNow, eightTime, 8 * interval, 9);
 }
 
-void switchLightBulb(int ledPin)
+void checkTimer(long currentTime, long lastTime, long interval, int ledPin)
 {
-	digitalWrite(ledPin, !digitalRead(ledPin));
+	if (currentTime - lastTime >= interval)
+	{
+		lastTime += interval;
+		digitalWrite(ledPin, !digitalRead(ledPin));
+		//Serial.println(currentTime);
+	}
 }
 
 void setUpPin(int pin)
