c50df43badcda2865a5f0b65df8ae3d25591733e
Peter Rosser <tharax@gmail.com>
2015-03-26 19:40:17 +0000

    more code

--- fast_then_slow.ino
+++ fast_then_slow.ino
@@ -1,77 +1,102 @@
-const int ledPin = 13;
-const int interval = 1000;
-int ledState = HIGH;
-long lastTime = 0;
-long doubleTime = 0;
-long quadTime = 0;
+/* Flashing LED Version 2
+ * ------------------------
+ *
+ * Turns on and off a light emitting diode(LED) connected to a digital  
+ * pin, in intervals of 2 seconds using millis() function
+ *
+ */
+int ledPin = 13;                 // LED connected to digital pin 13
+unsigned long currentTime;
+unsigned long loopTime;
 
 void setup()
 {
-	// for(int i=0; i<14; i++)
-	// {
-	// 	if(i % 2 == 1)
-	// 	{
-	// 		pinMode(i, OUTPUT);
-	// 	}
-	// }
-	pinMode(13, OUTPUT);
-	pinMode(11, OUTPUT);
-	pinMode(9, OUTPUT);
+  pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+  currentTime = millis();
+  loopTime = currentTime;  
+  Serial.begin(9600);
 }
 
 void loop()
 {
-	long timeNow = millis();
-	
-	if (timeNow - lastTime > interval)
-	{
-		lastTime = timeNow;
-		if(ledState == HIGH)
-		{
-			ledState = LOW;
-		}
-		else
-		{
-			ledState = HIGH;
-		}
-		// for(int i=0; i<14; i++)
-		// {
-		// 	if(i % 2 == 0)
-		// 	{
-				// digitalWrite(i, ledState);
-			// }
-			// if(i % 2 == 1)
-			// {
-				digitalWrite(13, ledState);
-				//digitalWrite(11, ledState);
-				//digitalWrite(9, ledState);
-		// 	}
-		// }
-	}
-	if (timeNow - doubleTime > (2 * interval))
-	{
-		doubleTime = timeNow;
-		if(ledState == HIGH)
-		{
-			ledState = LOW;
-		}
-		else
-		{
-			ledState = HIGH;
-		}
-		digitalWrite(11, ledState);
-	}
-	if (timeNow - quadTime > (4 * interval))
-	{
-		quadTime = timeNow;
-		if(ledState == HIGH)
-		{
-			ledState = LOW;
-		}
-		else
-		{
-			ledState = HIGH;
-		}
-		digitalWrite(9, ledState);
-	}
+  int val = !digitalRead(ledPin);
+  Serial.println(val);
+  digitalWrite(ledPin, val);   // toggles the LED on/off
+  delay(1000);                  // waits for a second
 }
+
+// const int ledPin = 13;
+// const int interval = 999;
+// int ledState = HIGH;
+// long lastTime = 0;
+// long doubleTime = 0;
+// long quadTime = 0;
+
+// void setup()
+// {
+// 	Serial.begin(9600);
+// 	pinMode(13, OUTPUT);
+// 	digitalWrite(13, LOW);
+// 	//pinMode(11, OUTPUT);
+// 	//pinMode(9, OUTPUT);
+// }
+
+// void loop()
+// {
+// 	long timeNow = millis();
+// 	if (timeNow - lastTime > interval)
+// 	{
+// 		lastTime = timeNow;
+// 		switchLightBulb(13);
+// 		Serial.println(lastTime);
+// 	}
+// 		// if(ledState == HIGH)
+// 		// {
+// 		// 	ledState = LOW;
+// 		// 	Serial.print("off: ");
+// 		// 	Serial.println(lastTime);
+// 		// }
+// 		// else
+// 		// {
+// 		// 	ledState = HIGH;
+// 		// 	Serial.print("on:  ");
+// 		// 	Serial.println(lastTime);
+// 		// }
+// 		// digitalWrite(13, ledState);
+// 	// }
+// 	// if (timeNow - doubleTime > (2 * interval))
+// 	// {
+// 	// 	doubleTime = timeNow;
+// 	// 	if(ledState == HIGH)
+// 	// 	{
+// 	// 		ledState = LOW;
+// 	// 		Serial.print("double off: ");
+// 	// 		Serial.println(doubleTime);
+// 	// 	}
+// 	// 	else
+// 	// 	{
+// 	// 		ledState = HIGH;
+// 	// 		Serial.print("double on:  ");
+// 	// 		Serial.println(doubleTime);
+// 	// 	}
+// 	// 	digitalWrite(11, ledState);
+// 	// }
+// 	// if (timeNow - quadTime > (4 * interval))
+// 	// {
+// 	// 	quadTime = timeNow;
+// 	// 	if(ledState == HIGH)
+// 	// 	{
+// 	// 		ledState = LOW;
+// 	// 	}
+// 	// 	else
+// 	// 	{
+// 	// 		ledState = HIGH;
+// 	// 	}
+// 	// 	digitalWrite(9, ledState);
+// 	// }
+// }
+
+// void switchLightBulb(int ledPin)
+// {
+// 	digitalWrite(ledPin, !digitalRead(ledPin));
+// }
